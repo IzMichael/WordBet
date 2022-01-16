e5ef826d65b6b4dd02746af28c016de74f2d50c2
IzMichael <themichael77@outlook.com>
2022-01-17 12:05:13 +1300

    Track played words

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,8 @@
 
     let input = '     ',
     all, simple,
-    allowinput, betinp;
+    allowinput, betinp,
+    page = 'game';
 
     onMount(async () => {
         input = '';
@@ -17,9 +18,7 @@
         all = await fetch('/assets/words.json').then(res => {return res.json()});
         simple = await fetch('/assets/simple.json').then(res => {return res.json()});
         
-        if (typeof window.localStorage.getItem('persistent') == Object) {
-            persistent = JSON.parse(window.localStorage.getItem('persistent'));
-        } else {
+        if (!window.localStorage.getItem('played') == true) {
             persistent = {
                 all: []
             };
@@ -31,6 +30,7 @@
                     played: false
                 })
             }
+            window.localStorage.setItem('played', 'true');
         };
 
         selectWord();
@@ -40,6 +40,10 @@
     function selectWord() {        
         gamedata.word = simple[Math.floor(Math.random() * ((simple.length-1) - 0 + 1) + 0)].toUpperCase();
         gamedata.wordid = [...persistent.all].filter(w => w.word == gamedata.word)[0].id;
+
+        if (persistent.all[persistent.all.findIndex(w => w.word == gamedata.word)].played == true) {
+            selectWord();
+        };
     }
     
     function sleep(s) {
@@ -82,8 +86,8 @@
         gamedata.bet = 7;
     }
 
-    let persistent;
-    $: window.localStorage.setItem('persistent', JSON.stringify(persistent))
+    let persistent = JSON.parse(window.localStorage.getItem('persistent')) || {};
+    $: window.localStorage.setItem('persistent', JSON.stringify(persistent));
 
     async function submit() {
         betinp.disabled = true;
@@ -126,6 +130,7 @@
 
                 gamedata.score += 5;
                 persistent.all[persistent.all.findIndex(w => w.word == gamedata.word)].played = true;
+                console.log(persistent.all[persistent.all.findIndex(w => w.word == gamedata.word)])
 
                 open();
             }
@@ -226,87 +231,97 @@
     </DialogContent>
 </DialogOverlay>
 
-<div class="flex flex-col items-center justify-start w-full h-full max-h-screen px-5 py-5 mx-auto lg:w-1/5 lg:px-0">
-    <div class="relative w-full">
-        <SvelteToast/>
+{#if page == 'game'}
+    <div class="flex flex-col items-center justify-start w-full h-full max-h-screen px-5 py-5 mx-auto lg:w-1/5 lg:px-0">
+        <div class="relative w-full">
+            <SvelteToast/>
+        </div>
+        
+        <h1 class="text-3xl font-bold font-work">WordBet #{gamedata.wordid}</h1>
+        <p class="w-full text-sm italic text-center">Created by IzMichael - Inspired by Wordle</p>
+
+        <div class="flex flex-row items-center justify-start w-full p-1 mb-2 border border-gray-500">
+            <div class="flex flex-row items-center justify-between flex-1">
+                <p class="font-semibold whitespace-nowrap">Your Bet: </p>
+                <input type="number" bind:value={gamedata.bet} bind:this={betinp} max=7 min=1 class="flex-1 pl-2 border-b border-black">
+            </div>
+
+            <div class="flex flex-row items-center justify-between flex-1">
+                <p class="flex-1 pr-2 font-semibold text-right whitespace-nowrap">Your Score: </p>
+                <p>{gamedata.score}</p>
+            </div>
+        </div>
+
+        <div class="relative flex flex-col items-center justify-start flex-1 w-full gap-2">
+            <div class="w-full border inputbg" style="border-color: transparent;">
+                <Row bind:word={gamedata.word} bind:value={input} input="true"></Row>
+            </div>
+
+            <hr class="w-full -my-1 transition-all duration-200 ease-linear border-gray-500" style="border-top-width: 0.125rem;">
+
+            {#each gamedata.tries as attempt, i}
+                <Row bind:word={gamedata.word} bind:value={attempt} bind:keys={gamedata.used} input="false"></Row>
+            {/each}
+
+            {#each {length: (gamedata.bet - gamedata.tries.length)} as _, i}
+                <Row bind:word={gamedata.word} value="     " bind:keys={gamedata.used} input="true"></Row>
+            {/each}
+
+            <hr class="absolute top-0 w-full transition-all duration-200 ease-linear border-red-500" style="border-top-width: 0.125rem; margin-top: {3 * gamedata.bet + 3}rem;">
+        </div>
+
+        <div class="w-full mt-10">
+            <div class="flex flex-col w-auto lg:left-0 keyboard-base lg:absolute lg:bottom-0 lg:w-full">
+                <div class="flex flex-row justify-between flex-1 max-w-full">
+                    <div class="key">Q</div>
+                    <div class="key">W</div>
+                    <div class="key">E</div>
+                    <div class="key">R</div>
+                    <div class="key">T</div>
+                    <div class="key">Y</div>
+                    <div class="key">U</div>
+                    <div class="key">I</div>
+                    <div class="key">O</div>
+                    <div class="key">P</div>
+                </div>
+                <div class="flex flex-row justify-between flex-1 max-w-full">
+                    <div class="key u"></div>
+                    <div class="key">A</div>
+                    <div class="key">S</div>
+                    <div class="key">D</div>
+                    <div class="key">F</div>
+                    <div class="key">G</div>
+                    <div class="key">H</div>
+                    <div class="key">J</div>
+                    <div class="key">K</div>
+                    <div class="key">L</div>
+                    <div class="key u green"></div>
+                </div>
+                <div class="flex flex-row justify-between flex-1 max-w-full">
+                    <div class="key">←</div>
+                    <div class="key u yellow"></div>
+                    <div class="key">Z</div>
+                    <div class="key">X</div>
+                    <div class="key">C</div>
+                    <div class="key">V</div>
+                    <div class="key">B</div>
+                    <div class="key">N</div>
+                    <div class="key">M</div>
+                    <div class="key u grey"></div>
+                    <div class="key">⏎</div>
+                </div>
+            </div>
+        </div>
     </div>
-    
-    <h1 class="text-3xl font-bold font-work">WordBet #{gamedata.wordid}</h1>
-    <p class="w-full text-sm italic text-center">Created by IzMichael - Inspired by Wordle</p>
-
-    <div class="flex flex-row items-center justify-start w-full p-1 mb-2 border border-gray-500">
-        <div class="flex flex-row items-center justify-between flex-1">
-            <p class="font-semibold whitespace-nowrap">Your Bet: </p>
-            <input type="number" bind:value={gamedata.bet} bind:this={betinp} max=7 min=1 class="flex-1 pl-2 border-b border-black">
-        </div>
-
-        <div class="flex flex-row items-center justify-between flex-1">
-            <p class="flex-1 pr-2 font-semibold text-right whitespace-nowrap">Your Score: </p>
-            <p>{gamedata.score}</p>
-        </div>
-    </div>
-
-    <div class="relative flex flex-col items-center justify-start flex-1 w-full gap-2">
-        <div class="w-full border inputbg" style="border-color: transparent;">
-            <Row bind:word={gamedata.word} bind:value={input} input="true"></Row>
-        </div>
-
-        <hr class="w-full -my-1 transition-all duration-200 ease-linear border-gray-500" style="border-top-width: 0.125rem;">
-
-        {#each gamedata.tries as attempt, i}
-            <Row bind:word={gamedata.word} bind:value={attempt} bind:keys={gamedata.used} input="false"></Row>
+{:else if page == 'list'}
+    <div class="flex flex-col items-center justify-start w-full h-full max-h-screen px-5 py-5 mx-auto font-mono lg:w-1/5 lg:px-0">
+        {#each persistent.all as w}
+            {#if w.played == true}
+                <p>{w.id} - {w.word}</p>
+            {/if}
         {/each}
-
-        {#each {length: (gamedata.bet - gamedata.tries.length)} as _, i}
-            <Row bind:word={gamedata.word} value="     " bind:keys={gamedata.used} input="true"></Row>
-        {/each}
-
-        <hr class="absolute top-0 w-full transition-all duration-200 ease-linear border-red-500" style="border-top-width: 0.125rem; margin-top: {3 * gamedata.bet + 3}rem;">
     </div>
-
-    <div class="w-full mt-10">
-        <div class="flex flex-col w-auto lg:left-0 keyboard-base lg:absolute lg:bottom-0 lg:w-full">
-            <div class="flex flex-row justify-between flex-1 max-w-full">
-                <div class="key">Q</div>
-                <div class="key">W</div>
-                <div class="key">E</div>
-                <div class="key">R</div>
-                <div class="key">T</div>
-                <div class="key">Y</div>
-                <div class="key">U</div>
-                <div class="key">I</div>
-                <div class="key">O</div>
-                <div class="key">P</div>
-            </div>
-            <div class="flex flex-row justify-between flex-1 max-w-full">
-                <div class="key u"></div>
-                <div class="key">A</div>
-                <div class="key">S</div>
-                <div class="key">D</div>
-                <div class="key">F</div>
-                <div class="key">G</div>
-                <div class="key">H</div>
-                <div class="key">J</div>
-                <div class="key">K</div>
-                <div class="key">L</div>
-                <div class="key u green"></div>
-            </div>
-            <div class="flex flex-row justify-between flex-1 max-w-full">
-                <div class="key">←</div>
-                <div class="key u yellow"></div>
-                <div class="key">Z</div>
-                <div class="key">X</div>
-                <div class="key">C</div>
-                <div class="key">V</div>
-                <div class="key">B</div>
-                <div class="key">N</div>
-                <div class="key">M</div>
-                <div class="key u grey"></div>
-                <div class="key">⏎</div>
-            </div>
-        </div>
-    </div>
-</div>
+{/if}
 
 <style global>
     @tailwind base;
