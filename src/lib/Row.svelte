fdb497aaf810a0b52a5b7db555f1970d28e7c6cc
IzMichael <themichael77@outlook.com>
2022-01-18 22:14:08 +1300

    Remove old redundant 'keys' variables from Row & Square components

--- src/lib/Row.svelte
+++ src/lib/Row.svelte
@@ -1,15 +1,15 @@
 <script>
     import Square from './Square.svelte';
 
-    export let word = '', value = '', input = 'false', keys;
+    export let word = '', value = '', input = 'false';
 </script>
 
-<div class="flex flex-row items-center justify-evenly w-full h-10 gap-2">
-    <Square bind:value={value[0]} word="{word}" role="0" input={input} bind:keys></Square>
-    <Square bind:value={value[1]} word="{word}" role="1" input={input} bind:keys></Square>
-    <Square bind:value={value[2]} word="{word}" role="2" input={input} bind:keys></Square>
-    <Square bind:value={value[3]} word="{word}" role="3" input={input} bind:keys></Square>
-    <Square bind:value={value[4]} word="{word}" role="4" input={input} bind:keys></Square>
+<div class="flex flex-row items-center w-full h-10 gap-2 justify-evenly">
+    <Square bind:value={value[0]} word="{word}" role="0" input={input}></Square>
+    <Square bind:value={value[1]} word="{word}" role="1" input={input}></Square>
+    <Square bind:value={value[2]} word="{word}" role="2" input={input}></Square>
+    <Square bind:value={value[3]} word="{word}" role="3" input={input}></Square>
+    <Square bind:value={value[4]} word="{word}" role="4" input={input}></Square>
 </div>
 
 <style>
--- src/lib/Square.svelte
+++ src/lib/Square.svelte
@@ -1,36 +1,21 @@
 <script>
-    export let value = '', word = '', role = 0, input = 'false', keys;
+    export let value = '', word = '', role = 0, input = 'false';
     let box;
     
     $: {
         if (box && input == 'false') {
             if (word[role] == value) {
                 box.style.backgroundColor = '#12cc0f';
-                keys.green = [...keys.green, value];
-                keys.green = keys.green.filter(function(item, pos, self) {
-                    return self.indexOf(item) == pos;
-                });
-                keys.green = keys.green;
             } else if (word.includes(value)) {
                 box.style.backgroundColor = '#dfe246';
-                keys.yellow = [...keys.yellow, value];
-                keys.yellow = keys.yellow.filter(function(item, pos, self) {
-                    return self.indexOf(item) == pos;
-                });
-                keys.yellow = keys.yellow;
             } else {
                 box.style.backgroundColor = '#a8a8a8';
-                keys.grey = [...keys.grey, value];
-                keys.grey = keys.grey.filter(function(item, pos, self) {
-                    return self.indexOf(item) == pos;
-                });
-                keys.grey = keys.grey;
             }
         }
     }
 </script>
 
-<h3 bind:this={box} class="flex flex-col items-center justify-center w-10 h-10 p-2 text-lg lg:text-3xl border border-gray-400" style="text-transform:uppercase">{value || ''}</h3>
+<h3 bind:this={box} class="flex flex-col items-center justify-center w-10 h-10 p-2 text-lg border border-gray-400 lg:text-3xl" style="text-transform:uppercase">{value || ''}</h3>
 
 <style>
     
